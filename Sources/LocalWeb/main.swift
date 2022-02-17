6549d1a5beb367dc551bab2b21d3d6c65bec2e7d
KeanuPang <keanu.pang@gmail.com>
2022-02-17 15:38:57 +0800

    Add logger for file downloading

--- Sources/LocalWeb/main.swift
+++ Sources/LocalWeb/main.swift
@@ -15,11 +15,13 @@ enum Server {
 
         app.middleware.add(DirectoryIndexMiddleware(root, application: app))
         app.middleware.add(HBFileMiddleware(root, application: app))
-//        app.middleware.add(HBLogRequestsMiddleware(.info))
+        app.middleware.add(HBLogRequestsMiddleware(.info))
 
         try app.start()
         app.wait()
     }
 }
 
-try Server.run(host: "127.0.0.1", port: 7920, root: "/Users/keanupang/Workshop/98. Temp/swift-web")
+let localIP = NetworkHelper.getIPAddress() ?? "127.0.0.1"
+
+try Server.run(host: "0.0.0.0", port: 7920, root: "/Users/keanupang/Workshop/98. Temp/swift-web")
